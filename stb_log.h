649008176a3aafc20d2944617cd92adea13dadf9
ycwang <wangyongcong@gmail.com>
2018-07-20 18:06:41 +0800

    async data handling
    mpmc async data handling
    thread-safe testing

--- stb_log.h
+++ stb_log.h
@@ -5,10 +5,13 @@
 #ifndef INCLUDE_STB_LOG_H
 #define INCLUDE_STB_LOG_H
 
-#include <array>
+#include <cstdlib>
+#include <cassert>
 #include <vector>
 #include <atomic>
+#include <thread>
 #include <new>
+#include <emmintrin.h>
 
 #ifdef USE_NAMESPACE
 namespace wyc {
@@ -24,34 +27,68 @@ namespace wyc {
 		NOTSET = 0,
 	};
 
-	constexpr size_t DefaultLogQueueSize = 1024;
-	template<typename T, size_t N = DefaultLogQueueSize >
-	class CLogRingBuffer {
-		static_assert((N > 0) && (N & (N - 1)) == 0, "CLogRingBuffer size must be positive power of 2");
-	
-	public:
-		T& operator[] (size_t i) {
-			return m_events[i & (N - 1)];
-		}
-
-		const T& operator[] (size_t i) const {
-			return m_events[i & (N - 1)];
-		}
-
-	private:
-		std::array<T, N> m_events;
-	};
-
 #ifndef CACHELINE_SIZE
 	#define CACHELINE_SIZE 64
 #endif
-#define PADDING_SIZE(s, p) (((s + p - 1) / p) * p - s)
+#define ASSERT_ALIGNMENT(ptr, align) assert((uintptr_t(ptr) % (align)) == 0)
+
+	struct alignas(CACHELINE_SIZE) Sequence
+	{
+		std::atomic<uint64_t> value;
+		char _padding[CACHELINE_SIZE - sizeof(std::atomic<uint64_t>)];
+
+		inline void set(uint64_t v) {
+			value = v;
+		}
+		inline uint64_t get() const {
+			return value;
+		}
+		inline uint64_t load() const {
+			return value.load(std::memory_order::memory_order_acquire);
+		}
+		inline void store(uint64_t v) {
+			value.store(v, std::memory_order::memory_order_release);
+		}
+		inline uint64_t fetch_add(uint64_t v) {
+			return value.fetch_add(v, std::memory_order::memory_order_relaxed);
+		}
+	};
+
+	void *aligned_alloc(size_t alignment, size_t size) 
+	{
+		// [Memory returned][ptr to start of memory][aligned memory][extra memory]
+		size_t request_size = size + alignment ;
+		void *raw = malloc(request_size + sizeof(void*));
+		if (!raw)
+			return nullptr;
+		void *ptr = (void**)raw + 1;
+		ptr = std::align(alignment, size, ptr, request_size);
+		if (!ptr) {
+			free(raw);
+			return nullptr;
+		}
+		*((void**)ptr - 1) = raw;
+		return ptr;
+	}
+
+	void aligned_free(void *ptr) 
+	{
+		void *raw = *((void**)ptr - 1);
+		free(raw);
+	}
 
 	struct LogEvent
 	{
 		int level;
-		char channel[32];
+		int size;
+		char channel[16];
+		union {
+			void *data;
+			char fixed_buffer[1];
+		};
+		Sequence publish;
 	};
+	constexpr unsigned log_event_fixed_buffer_size = offsetof(LogEvent, publish) - offsetof(LogEvent, data);
 
 	class ILogFilter
 	{
@@ -59,30 +96,79 @@ namespace wyc {
 		virtual ~ILogFilter() {}
 	};
 
+	class CLogger;
+
 	class ILogHandler
 	{
 	public:
+		static void* operator new(size_t size) {
+			return aligned_alloc(alignof(ILogHandler), size);
+		}
+		static void operator delete(void *ptr) {
+			aligned_free(ptr);
+		}
+
+		ILogHandler()
+			: m_logger(nullptr)
+			, m_filter_list(nullptr)
+		{
+			ASSERT_ALIGNMENT(this, CACHELINE_SIZE);
+			ASSERT_ALIGNMENT(&m_seq, CACHELINE_SIZE);
+			m_seq.set(0);
+		}
 		virtual ~ILogHandler() {}
 
+		inline void follow(const CLogger *seq) {
+			m_logger = seq;
+		}
+		inline uint64_t get_sequence() const {
+			return m_seq.get();
+		}
+		inline uint64_t acquire_sequence() const {
+			return m_seq.load();
+		}
+
+		void process();
+		virtual void process_event(const LogEvent *log) = 0;
+
 	private:
-		alignas(CACHELINE_SIZE) std::atomic<int64_t> m_seq;
-		alignas(CACHELINE_SIZE) ILogFilter *m_filter_list;
+		const CLogger* m_logger;
+		ILogFilter *m_filter_list;
+		Sequence m_seq;
 	};
 
 	class CLogger
 	{
 	public:
+		static void* operator new(size_t size) {
+			return aligned_alloc(alignof(CLogger), size);
+		}
+		static void operator delete(void *ptr) {
+			aligned_free(ptr);
+		}
+
 		CLogger(size_t buf_size);
 		~CLogger();
 		CLogger(const CLogger&) = delete;
 		CLogger& operator = (const CLogger&) = delete;
-		void write(int level, const char* channel, const char *message, ...);
+		void write(int level, const char* channel, const void *data, size_t size);
 		void add_handler(ILogHandler *handler);
-
+		void remove_handler(ILogHandler *handler);
+		const LogEvent* get_event(uint64_t seq) const {
+			return m_event_queue + (seq & m_size_mask);
+		}
+		LogEvent* get_event(uint64_t seq) {
+			return m_event_queue + (seq & m_size_mask);
+		}
+		
 	private:
-		alignas(CACHELINE_SIZE) std::atomic<int64_t> m_seq_claim;
-		alignas(CACHELINE_SIZE) LogEvent * m_event_queue;
-		ILogHandler *m_handler_list;
+		LogEvent * m_event_queue;
+		unsigned m_size_mask;
+		std::vector<ILogHandler*> m_handler_list;
+		uint64_t m_min_seq;
+		Sequence m_seq_claim;
+
+		uint64_t _claim(uint64_t count);
 	};
 
 #ifdef USE_NAMESPACE
@@ -97,12 +183,139 @@ namespace wyc {
 namespace wyc {
 #endif
 
-	CLogger::CLogger(size_t buf_size)
+	inline size_t get_next_power2(size_t val)
 	{
+		// val maybe power of 2
+		--val;
+		// set the bits right of MSB to 1
+		val |= (val >> 1);
+		val |= (val >> 2);
+		val |= (val >> 4);
+		val |= (val >> 8);		/* Ok, since int >= 16 bits */
+#if (SIZE_MAX != 0xffff)
+		val |= (val >> 16);		/* For 32 bit int systems */
+#if (SIZE_MAX > 0xffffffffUL)
+		val |= (val >> 32);		/* For 64 bit int systems */
+#endif // SIZE_MAX != 0xffff
+#endif // SIZE_MAX > 0xffffffffUL
+		++val;
+		assert((val & (val - 1)) == 0);
+		return val;
+	}
+
+	CLogger::CLogger(size_t size)
+	{
+		assert(size > 0);
+		if (size & (size - 1)) 
+			size = get_next_power2(size);
+		assert(sizeof(LogEvent) % CACHELINE_SIZE == 0);
+		size_t buf_size = sizeof(LogEvent) * size;
+		m_event_queue = (LogEvent*)aligned_alloc(CACHELINE_SIZE, buf_size);
+		assert(m_event_queue);
+		m_size_mask = size - 1;
+		for (size_t i = 0; i < size; ++i) {
+			LogEvent *log = &m_event_queue[i];
+			ASSERT_ALIGNMENT(log, CACHELINE_SIZE);
+			ASSERT_ALIGNMENT(&log->publish, CACHELINE_SIZE);
+			// initialize LogEvent
+			log->size = 0;
+			log->publish.set(0);
+		}
+		m_seq_claim.set(0);
+		m_min_seq = 0;
 	}
 
 	CLogger::~CLogger()
 	{
+		for (size_t i = 0; i <= m_size_mask; ++i) {
+			// clean up LogEvent
+			LogEvent *log = &m_event_queue[i];
+			if (log->data) {
+				//delete log->data;
+				log->data = nullptr;
+			}
+		}
+		aligned_free(m_event_queue);
+		m_event_queue = nullptr;
+	}
+
+	void CLogger::write(int level, const char* channel, const void *data, size_t size)
+	{
+		uint64_t seq = _claim(1);
+		// write event data
+		LogEvent *log = get_event(seq);
+		log->level = level;
+		unsigned last = sizeof(log->channel) - 1;
+		strncpy(log->channel, channel, last);
+		log->channel[last] = 0;
+		assert(size < log_event_fixed_buffer_size);
+		memcpy(&log->fixed_buffer, data, size);
+		log->size = size;
+		// publish event
+		log->publish.store(seq + 1);
+	}
+
+	uint64_t CLogger::_claim(uint64_t count) 
+	{
+		uint64_t request_seq = m_seq_claim.fetch_add(count);
+		if (request_seq < m_min_seq)
+			return request_seq;
+		uint64_t min_seq = ULLONG_MAX, seq;
+		for (ILogHandler *handler : m_handler_list) {
+			seq = handler->get_sequence();
+			while (request_seq > seq + m_size_mask) {
+				_mm_pause(); // pause, about 12ns
+				seq = handler->get_sequence();
+				if (request_seq <= seq + m_size_mask)
+					break;
+				// if no waiting threads, about 113ns
+				// else lead to thread switching
+				std::this_thread::yield();
+				seq = handler->get_sequence();
+			}
+			seq = handler->acquire_sequence();
+			if (seq < min_seq)
+				min_seq = seq;
+		}
+		m_min_seq = seq;
+		return request_seq;
+	}
+
+	void CLogger::add_handler(ILogHandler * handler)
+	{
+		m_handler_list.push_back(handler);
+		handler->follow(this);
+	}
+
+	void CLogger::remove_handler(ILogHandler * handler)
+	{
+		for (auto iter = m_handler_list.begin(); iter != m_handler_list.end(); ++iter) {
+			if (*iter == handler)
+			{
+				m_handler_list.erase(iter);
+				handler->follow(nullptr);
+				return;
+			}
+		}
+	}
+
+	void ILogHandler::process() 
+	{
+		assert(m_logger);
+		uint64_t seq = m_seq.get(), pub;
+		const LogEvent *log;
+		while (true) {
+			log = m_logger->get_event(seq);
+			pub = log->publish.load();
+			if (pub > seq) {
+				process_event(log);
+				m_seq.store(pub);
+				seq += 1;
+				assert(pub == seq);
+				continue;
+			}
+			break;
+		}
 	}
 
 #ifdef USE_NAMESPACE
--- test_log.cpp
+++ test_log.cpp
@@ -1,8 +1,140 @@
 #include <stdio.h>
+#include <time.h>
+#include <random>
+#define STB_LOG_IMPLEMENTATION
 #include "stb_log.h"
 
+using namespace wyc;
+
+class CLogWriter: public ILogHandler
+{
+public:
+	CLogWriter(int max_size, unsigned level, std::atomic<int> *counter)
+		: m_level(level)
+		, m_counter(counter)
+	{
+		m_logs.reserve(max_size);
+	}
+
+	virtual void process_event(const LogEvent *log) override
+	{
+		if (m_level == log->level)
+		{
+			m_logs.push_back(*(int*)(log->fixed_buffer));
+			m_counter->fetch_sub(1, std::memory_order::memory_order_relaxed);
+		}
+	}
+
+	inline std::vector<unsigned> get_logs() {
+		return m_logs;
+	}
+
+private:
+	unsigned m_level;
+	std::vector<unsigned> m_logs;
+	std::atomic<int> *m_counter;
+};
+
+void common_test()
+{
+	printf("stb_log common test\n");
+	printf("core: %d\n", std::thread::hardware_concurrency());
+
+	constexpr int channel_count = 2;
+	const char* channel_name[channel_count] = {
+		"C1", "C2"
+	};
+	const StbLogLevel channel_level[channel_count] = {
+		StbLogLevel::DEBUG,
+		StbLogLevel::INFO,
+	};
+	constexpr int max_write_count = 100000;
+
+	alignas(CACHELINE_SIZE) std::atomic<int> ch1_counter = 1;
+	alignas(CACHELINE_SIZE) std::atomic<int> ch2_counter = 1;
+	alignas(CACHELINE_SIZE) std::atomic<int> max_read_count = max_write_count * 2;
+
+	std::atomic<int> *channel_counter[channel_count] = {
+		&ch1_counter, &ch2_counter
+	};
+
+	CLogger *logger = new CLogger(256);
+	CLogWriter *h1 = new CLogWriter(max_write_count, channel_level[0], &max_read_count);
+	logger->add_handler(h1);
+	CLogWriter *h2 = new CLogWriter(max_write_count, channel_level[1], &max_read_count);
+	logger->add_handler(h2);
+
+	std::thread t1([&] {
+		while (max_read_count > 0) {
+			h1->process();
+			std::this_thread::yield();
+		}
+	});
+
+	std::thread t2([&] {
+		while (max_read_count > 0) {
+			h2->process();
+			std::this_thread::yield();
+		}
+	});
+
+	std::thread t3([&] {
+		std::mt19937 random((unsigned)time(NULL));
+		for(int cnt=0; cnt < max_write_count; ++cnt)
+		{
+			auto i = random() % channel_count;
+			auto c = channel_counter[i]->fetch_add(1, std::memory_order::memory_order_relaxed);
+			logger->write(channel_level[i], channel_name[i], &c, sizeof(c));
+		}
+	});
+
+	std::thread t4([&] {
+		std::mt19937 random((unsigned)time(NULL));
+		for (int cnt = 0; cnt < max_write_count; ++cnt)
+		{
+			auto i = random() % channel_count;
+			auto c = channel_counter[i]->fetch_add(1, std::memory_order::memory_order_relaxed);
+			logger->write(channel_level[i], channel_name[i], &c, sizeof(c));
+		}
+	});
+
+	printf("please wait...\n");
+	t1.join();
+	t2.join();
+	t3.join();
+	t4.join();
+	
+	// validation
+	printf("validate result...\n");
+	assert(max_read_count == 0);
+	auto &log1 = h1->get_logs();
+	auto &log2 = h2->get_logs();
+	printf("channel[1] = %d\n", log1.size());
+	printf("channel[2] = %d\n", log2.size());
+	assert(log1.size() + log2.size() == max_write_count * 2);
+	assert(log1.size() == ch1_counter - 1);
+	assert(log2.size() == ch2_counter - 1);
+	std::sort(log1.begin(), log1.end());
+	for (unsigned i = 0; i < log1.size(); ++i) {
+		assert(i + 1 == log1[i]);
+	}
+	std::sort(log2.begin(), log2.end());
+	for (unsigned i = 0; i < log2.size(); ++i) {
+		assert(i + 1 == log2[i]);
+	}
+
+	// clear up
+	logger->remove_handler(h1);
+	logger->remove_handler(h2);
+	delete h1;
+	delete h2;
+	delete logger;
+	printf("Success\n");
+}
 
 int main(int args, char *argv[])
 {
-	printf("test stb_log");
+	common_test();
+	getchar();
+	return 0;
 }
